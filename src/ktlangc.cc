58c44f2fdd0da08346f6a14b4dc27eb2d5d2267a
Matvey Arye <mat@cloudflare.com>
2013-06-10 17:20:00 -0700

    about to test

---
+++ src/ktlangc.cc
@@ -0,0 +1,103 @@
+/*************************************************************************************************
+ * C language binding
+ *                                                               Copyright (C) 2009-2012 FAL Labs
+ * This file is part of Kyoto Cabinet.
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version
+ * 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************************************/
+
+
+#include <ktremotedb.h>
+#include "ktlangc.h"
+#include "postgres.h"
+
+using namespace kyototycoon;
+
+extern "C" {
+
+/**
+ * Create a database object.
+ */
+KTDB* ktdbnew(void) {
+  _assert_(true);
+  return (KTDB*)new RemoteDB;
+}
+
+
+/**
+ * Destroy a database object.
+ */
+void ktdbdel(KTDB* db) {
+  _assert_(db);
+  RemoteDB* pdb = (RemoteDB*)db;
+  delete pdb;
+}
+
+
+/**
+ * Open a database file.
+ */
+int32_t ktdbopen(KTDB* db, const char* host, int32_t port, double timeout) {
+  _assert_(db && host && port && timeout);
+  RemoteDB* pdb = (RemoteDB*)db;
+  return pdb->open(host, port, timeout);
+}
+
+
+/**
+ * Close the database file.
+ */
+int32_t ktdbclose(KTDB* db) {
+  _assert_(db);
+  RemoteDB* pdb = (RemoteDB*)db;
+  return pdb->close();
+}
+
+int64_t ktdbcount(KTDB* db) {
+  _assert_(db);
+  RemoteDB* pdb = (RemoteDB*)db;
+  return pdb->count();
+}
+
+KTCUR* get_cursor(KTDB* db) {
+  _assert_(db);
+  RemoteDB* pdb = (RemoteDB*)db;
+  RemoteDB::Cursor *cur = pdb->cursor();
+  cur->jump();
+  return (KTCUR*) cur;
+}
+
+void ktcurdel(KTCUR* cur) {
+  _assert_(cur);
+  RemoteDB::Cursor *rcur = (RemoteDB::Cursor *) cur;
+  delete rcur;
+}
+
+
+bool next(KTDB* db, KTCUR* cur, char **key, char **value)
+{
+  std::string skey;
+  std::string sval;
+
+  RemoteDB::Cursor *rcur = (RemoteDB::Cursor *) cur;
+  bool res = rcur->get(&skey, &sval, NULL, true);
+  if(!res)
+    return false;
+
+  *key = (char *) palloc(sizeof(char)*(skey.length()+1));
+  *value = (char *) palloc(sizeof(char)*(sval.length()+1));
+
+  std::strcpy(*key, skey.c_str());
+  std::strcpy(*value, sval.c_str());
+  return true;
+}
+
+}
+
+// END OF FILE
---
+++ src/ktlangc.h
@@ -0,0 +1,77 @@
+/*************************************************************************************************
+ * C language binding
+ *                                                               Copyright (C) 2009-2012 FAL Labs
+ * This file is part of Kyoto Cabinet.
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version
+ * 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************************************/
+
+
+#ifndef _KTLANGC_H                       /* duplication check */
+#define _KTLANGC_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if !defined(__STDC_LIMIT_MACROS)
+#define __STDC_LIMIT_MACROS  1           /**< enable limit macros for C++ */
+#endif
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+
+/**
+ * C wrapper of polymorphic database.
+ */
+typedef struct {
+  void* db;                              /**< dummy member */
+} KTDB;
+
+typedef struct {
+  void* cur;                             /**< dummy member */
+} KTCUR;
+
+
+KTDB* ktdbnew(void);
+void ktdbdel(KTDB* db);
+int32_t ktdbopen(KTDB* db, const char* host, int32_t port, double timeout);
+int32_t ktdbclose(KTDB* db);
+
+
+int64_t ktdbcount(KTDB* db);
+
+KTCUR* get_cursor(KTDB* DB);
+void ktcurdel(KTCUR* cur);
+
+bool next(KTDB* db, KTCUR* cur, char **key, char **value);
+
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif                                   /* duplication check */
+
+/* END OF FILE */
